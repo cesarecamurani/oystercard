ca226ad38532a80dee5eb226024131c98f6ab639
cesare camurani <cesare.camurani@gmail.com>
2018-11-05 17:40:20 +0000

    limit exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,22 @@ class Oystercard
 
 attr_reader :balance
 
+MAX_BALANCE = 90
+
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    limit_exceeded?(amount)
     @balance += amount
-    @balance
   end
+
+  private
+
+  def limit_exceeded?(amount)
+    raise "Max limit is £90" if (@balance + amount) > MAX_BALANCE
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,14 +2,17 @@ require "oystercard"
 
 describe Oystercard do
 
-  it "set the default balance to 0" do
-    expect(subject.balance).to eq 0
-  end
+  it { expect(subject.balance).to eq 0 }
 
   describe "#top_up" do
     it { is_expected.to respond_to(:top_up).with(1).argument }
     it { expect{ subject.top_up 1 }.to change{ subject.balance }.by 1 }
   end
-  
+
+  it "raise an error if balance exceed the limit of £90" do
+    expect { subject.top_up(100) }.to raise_error "Max limit is £90"
+  end
+
+
 
 end
