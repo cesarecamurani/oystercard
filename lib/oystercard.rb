fae2e82e4c161a9878cf5c94d23d949a4742894b
cesare camurani <cesare.camurani@gmail.com>
2018-11-05 23:27:11 +0000

    track entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,10 @@
 class Oystercard
 
-attr_reader :balance, :moving
+attr_reader :balance, :moving, :entry_station
 
 MAX_BALANCE = 90
 MIN_BALANCE = 1
+MIN_FARE = 1
 
   def initialize(balance = 0, moving = false)
     @balance = balance
@@ -15,27 +16,30 @@ MIN_BALANCE = 1
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
-  def touch_in(credit)
-    no_credit?(credit)
+  def touch_in(entry_station)
+    no_credit?
+    @entry_station = entry_station
     @moving = true
   end
 
-  def touch_out
+  def touch_out(entry_station)
+    deduct(MIN_FARE)
+    @entry_station = nil
     @moving = false
   end
 
   private
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
   def limit_exceeded?(amount)
     raise "Max limit is £90" if (@balance + amount) > MAX_BALANCE
   end
 
-  def no_credit?(credit)
-    raise "Not enough credit" if (@balance + credit) < MIN_BALANCE
+  def no_credit?
+    raise "Not enough credit" if @balance < MIN_BALANCE
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,8 @@
 require "oystercard"
 
 describe Oystercard do
-  let(:oystercard) { described_class.new }
+  let(:oystercard){ described_class.new }
+  let(:entry_station){ double :entry_station }
 
   it { expect(oystercard.balance).to eq 0 }
   it { expect(oystercard.moving).to eq false }
@@ -15,25 +16,28 @@ describe Oystercard do
     end
   end
 
-  describe "#deduct" do
-    it "deduct the amount from the balance" do
-      expect{ oystercard.deduct 1 }.to change{ oystercard.balance }.by -1
-    end
-  end
-
   describe "#touch_in" do
     it "allow the passenger to enter the tube" do
-      expect{ oystercard.touch_in(1) }.to change{ oystercard.moving }.to true
+      oystercard.top_up(1)
+      expect{ oystercard.touch_in(entry_station) }.to change{ oystercard.moving }.to true
+    end
+    it "track the entry station" do
+      oystercard.top_up(1)
+      oystercard.touch_in(entry_station)
+      expect(oystercard.entry_station).to eq entry_station
     end
     it "raise an error if the credit is less than £1" do
-      expect{ oystercard.touch_in(0) }.to raise_error "Not enough credit"
+      expect{ oystercard.touch_in(entry_station) }.to raise_error "Not enough credit"
     end
   end
 
   describe "#touch_out" do
     it "allow the passenger to leave the tube" do
-      oystercard.touch_in(1)
-      expect{ oystercard.touch_out }.to change{ oystercard.moving }.to false
+      oystercard.top_up(1)
+      oystercard.touch_in(entry_station)
+      expect{ oystercard.touch_out(entry_station) }.to change{ oystercard.moving }.to false
+      expect{ oystercard.touch_out(entry_station) }.to change{ oystercard.balance }.by(-Oystercard::MIN_FARE)
+      expect(oystercard.entry_station).to eq nil
     end
   end
 
