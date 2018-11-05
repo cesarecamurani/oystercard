9a4a45fede40baba52a8f15525381a4d43ed99a6
cesare camurani <cesare.camurani@gmail.com>
2018-11-05 16:31:47 +0000

    top up added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require "oystercard"
+
 describe Oystercard do
+
+  it "set the default balance to 0" do
+    expect(subject.balance).to eq 0
+  end
+
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it { expect{ subject.top_up 1 }.to change{ subject.balance }.by 1 }
+  end
   
+
 end
